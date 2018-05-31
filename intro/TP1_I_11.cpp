ef29ea8353d1c0835dbe6d96befdb521b3e37220
David Tupac Yupanqui <37391249+dtupac@users.noreply.github.com>
2018-05-30 23:01:20 -0300

    Update TP1_I_11.cpp

--- intro/TP1_I_11.cpp
+++ intro/TP1_I_11.cpp
@@ -1,26 +1,20 @@
-/*
 TP1-I-11:
 Desarrollar un programa en el cual introduzca un número entero 
 positivo e invierta los dígitos del número. Mostrar en pantalla el número invertido.
-*/
 
 #include<iostream>
 #include<conio.h>
-#include<stdio.h>
-#include<string.h>
 using namespace std;
 
 int main(){
-	char n[10];
-	int largo;
-	cout<<"Introduce un numero entero positivo: ";cin>>n;
-	largo = strlen(n);
-	cout<<"El numero invertido es:";
-	do{
-		cout<<n[largo];
-		largo--;
-	}while(largo >= 0);
-		
+	int num,resto,numINV=0;
+  	cout<<"Introduce un numero entero positivo: ";cin>>num;
+ 	while(num>0){
+        resto=num%10;
+    	num=num/10;
+      	numINV=numINV*10+resto;
+ 	}
+ 	cout<<"\nEl numero invertido es: "<<numINV;
 	getch();
 	return 0;
 }
